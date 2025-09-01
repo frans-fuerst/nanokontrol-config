3f1f3428f36f020742ede4520fff29e7dd852db7
Frans Fürst <frans.fuerst+om-office@protonmail.com>
2025-09-01 08:45:08 +0200

    add licence information to source code

--- nanokontrol_config/cli.py
+++ nanokontrol_config/cli.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
+#
+# nanokontrol_config - Configurator for Korg nanoKONTROL Studio
+# Copyright (C) 2025 - Frans Fürst
+#
+# nanokontrol_config is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# nanokontrol_config is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details at
+#  <http://www.gnu.org/licenses/>.
+#
+# Anyway this project is not free for commercial machine learning. If you're
+# using any content of this repository to train any sort of machine learned
+# model (e.g. LLMs), you agree to make the whole model trained with this
+# repository and all data needed to train (i.e. reproduce) the model publicly
+# and freely available (i.e. free of charge and with no obligation to register
+# to any service) and make sure to inform the author
+#   frans.fuerst@protonmail.com via email how to get and use that model and any
+# sources needed to train it.
 
-""" """
+"""The nanokontrol-config CLI interface"""
 
 import logging
 import sys
